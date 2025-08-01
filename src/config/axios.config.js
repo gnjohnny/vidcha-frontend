4e2d0e1f579fec44a98b8e22566415e44b6001c1
John <54johnmbugua@gmail.com>
2025-08-01 19:24:33 +0300

    Modified axios configuration

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://vidcha-api.onrender.com",
+    baseURL: "https://vidcha-api.onrender.com/api",
     withCredentials: true,
 });
